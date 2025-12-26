ae240cefe6a30a5db55e674fc68d078d1931462e
juandelsur5 <juandelsur8@gmail.com>
2025-12-26 17:03:07 -0500

    fix: valid manifest to avoid syntaxerror

---
+++ eco_city_catalog_v2/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Eco City Catalog v2',
+    'version': '17.0.1.0.0',
+    'summary': 'Catálogo jerárquico de ubicación (país, departamento, ciudad, barrio)',
+    'category': 'Contacts',
+    'author': 'Juandelsur',
+    'license': 'LGPL-3',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'security/eco_city_security.xml',
+        'security/ir.model.access.csv',
+        'views/eco_city_views.xml',
+        'views/eco_neighborhood_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
